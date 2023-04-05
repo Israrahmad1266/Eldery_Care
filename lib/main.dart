1603f79a46619fb98814f9799281480ea6e8b317
israrahmad1266 <israarahmaad1266@gmail.com>
2023-04-05 21:43:10 +0530

    changes in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,4 @@
-import 'package:doctor_project/Home/navigationbar.dart';
 import 'package:doctor_project/Home/home1.dart';
-import 'package:doctor_project/HomePage%20Menu/home_menu.dart';
-import 'package:doctor_project/Home_Page/home_page.dart';
 
 import 'package:flutter/material.dart';
 
@@ -12,12 +9,8 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Flutter Demo',
-        home: HomeScreen());
+    return MaterialApp(debugShowCheckedModeBanner: false, home: HomeScreen());
   }
 }
